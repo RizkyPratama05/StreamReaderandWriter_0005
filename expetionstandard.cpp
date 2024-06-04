456da4f57e9c69f8161d1fd9a1ce450704c789eb
RizkyPratama05 <rizkypp428@gmail.com>
2024-06-04 18:27:37 +0700

    exeption standard

---
+++ expetionstandard.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <exception>
+#include <array>
+using namespace std;
+
+int main()
+{
+    cout << "Awal Program" << endl; 
+    try
+    {
+        array<int, 3> data = { 3, 5, 7 };
+        cout << data.at(1) << endl;
+    }
+
+    catch (exception& e)
+    {
+        cout << e.what() << endl;
+    }
+
+    cout << "Baris Program yang terakhir" << endl;
+    return 0;
+}
