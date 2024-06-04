dffd2425362ecc7da4128778e14ea1b35af4158e
RizkyPratama05 <rizkypp428@gmail.com>
2024-06-04 17:37:02 +0700

    if, while, else

--- ioFile.cpp
+++ ioFile.cpp
@@ -26,4 +26,18 @@ int main() {
     infile.open("contohfile.txt");
 
     cout << endl << ">= Membuka dan membaca file " << endl;
+
+    if (infile.is_open())
+    {
+        while (getline(infile, baris))
+        {
+            cout << baris << '\n';
+        }
+
+        infile.close();
+    }
+
+    else cout << "Unable to open file";
+    return 0;
+} 
     
