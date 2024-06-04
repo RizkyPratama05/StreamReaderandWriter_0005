e8804a6936ddc3a202132e282619769a104cc18f
RizkyPratama05 <rizkypp428@gmail.com>
2024-06-04 17:29:43 +0700

    while true

--- ioFile.cpp
+++ ioFile.cpp
@@ -12,4 +12,11 @@ int main() {
 
     cout << ">=Menulis file, \'q\' untuk keluar" << endl;
 
-    
+    while(true) {
+        cout << "-";
+        getline (cin, baris);
+        if (baris == "q") break;
+        outfile << baris << endl;
+    }
+
+   
